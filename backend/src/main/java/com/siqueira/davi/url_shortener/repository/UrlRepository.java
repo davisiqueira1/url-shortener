15c19689b4a65ef12587d1e24c15dc5e7de4bb1b
Davi Siqueira <davisvsiqueira@gmail.com>
2025-08-07 15:54:23 -0300

    feat: add url repository

---
+++ backend/src/main/java/com/siqueira/davi/url_shortener/repository/UrlRepository.java
@@ -0,0 +1,12 @@
+package com.siqueira.davi.url_shortener.repository;
+
+import com.siqueira.davi.url_shortener.model.UrlModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UrlRepository extends MongoRepository<UrlModel, String> {
+    Optional<UrlModel> findByShortUrl(String shortUrl);
+}
---
+++ backend/src/main/java/com/siqueira/davi/url_shortener/service/UrlService.java
@@ -0,0 +1,4 @@
+package com.siqueira.davi.url_shortener;
+
+public class UrlService {
+}
