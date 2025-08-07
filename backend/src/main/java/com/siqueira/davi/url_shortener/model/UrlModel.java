a0ab8e8140b92c9be300fb8a3dd8bc14f1adee16
Davi Siqueira <davisvsiqueira@gmail.com>
2025-08-07 15:48:55 -0300

    feat: add url model

---
+++ backend/src/main/java/com/siqueira/davi/url_shortener/model/UrlModel.java
@@ -0,0 +1,26 @@
+package com.siqueira.davi.url_shortener.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "url")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UrlModel {
+
+    @Id
+    private String id;
+
+    private String longUrl;
+    private String shortUrl;
+
+    private LocalDateTime expirationDate;
+}
